539b103b982d8fdfb627b4cbac948abacd56e239
Matthew Fisher <mwfisher91@gmail.com>
2021-09-24 09:46:05 -0700

    update

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -5,18 +5,18 @@ class Stack:
 
     # Always
     def push(self, item):
-        # print(f"Pushing: {item}")
+        print(f"Pushing: {item}")
         self.data.append(item)
 
     # Always
     def pop(self):
         popped = self.data.pop(-1)
-        # print(f"Popping: {popped}")
+        print(f"Popping: {popped}")
         return popped
 
     # Optional
     def peek(self):
-        # print("Peeking: ")
+        print("Peeking: ")
         return self.data[-1]
 
     def getLength(self):
@@ -33,25 +33,7 @@ class Stack:
 # has balanced brackets
 # should return True or False
 def balancedBrackets(code):
-    opening = ["(", "{", "["]
-    # opening = {"(": True, "{": True, "[": True}
-    # if you assume no other letters then you can use an object
-    closing = {")": "(", "}": "{", "]": "["}
-
-    stack = Stack()
-
-    for char in code:
-        if char in opening:
-            stack.push(char)
-
-        if char in closing:
-            if not stack.getLength():
-                return False
-            lastChar = stack.pop()
-            if lastChar != closing[char]:
-                return False
-
-    return not stack.getLength()
+    pass
 
 
 print(balancedBrackets("{age(cz{gge(gea[aa]gadz)}gea)}"))  # True
